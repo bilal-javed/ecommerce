3104f0fb30fc22f8c80d3d2fc6da5b58dddc642a
bilal-javed <bjaved51@gmail.com>
2015-12-16 12:48:10 +0500

    Created a new project and generate product scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecommerce::Application.config.secret_token = '11bd3a996f3e0c35e579226ae1ea52adb803678275e1d578c115daba83606dc1636b193551654c6a2e7606119dd112abb1ce1351eed9e2079617cba864bd2a04'
